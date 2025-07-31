afc087b388e39eb5d993e67efd7d5b398d23d858
Muzamil <184967197+muzi5622@users.noreply.github.com>
2025-08-01 00:42:06 +0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  output: "static", // ✅ Add this line
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
